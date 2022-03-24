27b0587c58254dcb576927a43792d9415ce1c3bc
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-03-24 09:08:14 +0000

    P8

--- p8original.c
+++ p8original.c
@@ -16,13 +16,14 @@ Triangle input_triangle()
   Triangle t;
   printf("Enter the base and altitude of the triangle\n");
   scanf("%f%f",&t.base,&t.altitude);
+  return t;
 }
 void input_n_triangles(int n, Triangle t[n])
 {
   int i;
-  for(i=0;i<n;i--)
+  for(i=0;i<n;i++)
     {
-      t[n]=input_triangle();
+      t[i]=input_triangle();
     }
 }
 void find_area(Triangle *t)
@@ -31,5 +32,13 @@ void find_area(Triangle *t)
 }
 void find_areas_n(int n, Triangle t[n])
 {
+  
+}
+int main()
+{
+  int n;
+  n=input_n();
+  Triangle t[n];
+  t[n]=input_triangle();
   
 }
