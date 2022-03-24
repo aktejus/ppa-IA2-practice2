4b84ac69182552977e0d891f8e03c70c47e0bbed
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-03-24 10:17:20 +0000

    P8

--- p8original.c
+++ p8original.c
@@ -26,19 +26,46 @@ void input_n_triangles(int n, Triangle t[n])
       t[i]=input_triangle();
     }
 }
-void find_area(Triangle *t)
+Triangle find_area(Triangle *t)
 {
   t->area=0.5*t->base*t->altitude;
+  return *t;
 }
 void find_areas_n(int n, Triangle t[n])
 {
-  
+  for(int i;i<n;i++)
+    {
+      find_area(&t[i]);
+    }
+}
+Triangle find_smallest_triangle(int n, Triangle t[n])
+{
+  Triangle small;
+small=t[0];
+  for(int i=0;i<n;i++)
+  if(small.area>t[i].area)
+  {
+    small=t[i];
+  }
+return small;
+void output(int n, Triangle t[n], Triangle smallest)
+{
+  for(int i=0;i<n;i++)
+    {
+      printf("%f, %f and\n", t[i].base, t[i].altitude);
+  }
+  printf("%f, %f\nis\ntriangle with base = %f and altitude = %f 
+          having area = %f\n", t[n-1].base, t[n-1].altitude, 
+          smallest.base, smallest.altitude, smallest.area);
 }
 int main()
 {
   int n;
   n=input_n();
-  Triangle t[n];
-  t[n]=input_triangle();
-  
-}
+  Triangle t[n],small;
+  input_n_triangles(n, t);
+  find_areas_n(n,t);
+  small=find_smallest_triangle(n,t);
+  output(n,t,small);
+  return 0;
+  }
