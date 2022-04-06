8ed4504f32d76071333444997ca380fe40cc50c8
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-04-06 16:15:52 +0000

    Simple interest

---
+++ si.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int p,t;
+  float r,si;
+  printf("Enter the p,r and t\n");
+  scanf("%d %f %d",&p,&r,&t);
+  si=p*r*t;
+  printf("The si is %f",si);
+  return 0;
+}
